73e6eacb1c65f9e72e928f73bca744fc0672f15a
Ben Lodge <blodge@hubspot.com>
2016-08-18 10:51:36 -0400

    allow multiple editors to be used on one page

--- demo/scripts/demo.js
+++ demo/scripts/demo.js
@@ -9,19 +9,26 @@ module.exports = React.createClass({
 
   getInitialState() {
     return {
-      textValue: "I am the initial value. Erase me, or try the button above."
+      textValue1: "I am the initial value. Erase me, or try the button above.",
+      textValue2: "I am another initial value"
     }
   },
 
-  handleEditorChange(value) {
+  handleChange1(value) {
     this.setState({
-      textValue: value
+      textValue1: value
+    });
+  },
+
+  handleChange2(value) {
+    this.setState({
+      textValue2: value
     });
   },
 
   handleTextChange() {
     this.setState({
-      textValue: `Changing text ${counter++}`
+      textValue1: `Changing text by setting new state. ${counter++}`
     });
   },
 
@@ -33,19 +40,23 @@ module.exports = React.createClass({
             <a href="https://github.com/benrlodge/react-simplemde-editor">react-simplemde-editor</a>
           </h1>
           <p className="lead">
-            A React.js wrapper for <a href="https://github.com/NextStepWebs/simplemde-markdown-editor">simplemde-markdown-editor</a>. 
+            A React.js wrapper for <a href="https://github.com/NextStepWebs/simplemde-markdown-editor">simplemde-markdown-editor</a>.
           </p>
         </div>
         <button style={{display: "inline-block", margin: "10px 0"}} onClick={this.handleTextChange}>
           Click me to update the textValue outside of the editor
         </button>
-        <Editor 
-          value={this.state.textValue}
-          handleEditorChange={this.handleEditorChange} />
-        <h5>this.state.textValue:</h5>
-        <pre>
-          {this.state.textValue}
-        </pre>
+        <Editor
+          value={this.state.textValue1}
+          handleEditorChange={this.handleChange1}
+        />
+        <pre>Value 1:{this.state.textValue1}</pre>
+        <hr />
+        <Editor
+          value={this.state.textValue2}
+          handleEditorChange={this.handleChange2}
+        />
+        <pre>Value 2:{this.state.textValue2}</pre>
       </div>
     )
   }
