20a9731ddb426410bf43b841ee572afa09177bcf
Ben Lodge <blodge@hubspot.com>
2016-08-30 21:21:30 -0400

    add dist directory

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var webpack = require('webpack');
+var isProd = (process.env.NODE_ENV === 'production');
+
+module.exports = {
+  node: { fs: 'empty' },
+  entry: './src/index.js',
+  devtool: 'source-map',
+  output: {
+    path: './dist',
+    filename: isProd ? 'react-simplemde-editor.min.js' : 'react-simplemde-editor.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel",
+        query:
+          {
+            presets:['react', 'es2015']
+          }
+      }
+    ]
+  }
+
+};
